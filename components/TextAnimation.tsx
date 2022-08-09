c10e44db1b36f621b2f9b4b7a2abde470e532e91
Kathleen Kang <kathleenkang@Kathleens-MacBook-Air.local>
2022-08-10 03:30:38 +0900

    fix changes

--- components/TextAnimation.tsx
+++ components/TextAnimation.tsx
@@ -1,19 +1,11 @@
 import styles from "./TextAnimation.module.css";
 
-<<<<<<< HEAD
 type TextAnimationTypes = {
   voteCount: number;
 };
 
 // export function TextAnimation({ voteCount }: number) {
 export function TextAnimation({ voteCount }: TextAnimationTypes) {
-=======
-type Props = {
-  voteCount: number;
-};
-
-export function TextAnimation({ voteCount }: Props) {
->>>>>>> 48201b2427d880218b4fab659939972c161444ad
   return (
     <div className={`${styles.revealText}`}>
       👍
